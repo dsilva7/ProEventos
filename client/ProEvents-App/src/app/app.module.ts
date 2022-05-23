efdd41c769d5eafa48fdc9752508ab831ca44f08
dsilva7 <45966938+dsilva7@users.noreply.github.com>
2022-05-23 08:59:07 -0300

    Seção 4: Angular

--- client/ProEvents-App/src/app/app.module.ts
+++ client/ProEvents-App/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { SpeakerComponent } from './speaker/speaker.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { SpeakerComponent } from './speaker/speaker.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
